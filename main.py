6622445f3c6034774f73492f2edd9425c6816a3b
ItsanOriot <66038519+ItsanOriot@users.noreply.github.com>
2023-04-06 14:16:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -26,13 +26,26 @@ def getPassCreator(passid: int):
     passData = requests.get(passString).json()  
     return passData['Creator']['Id']
 
+def getCodes(price: int):
+    count = 0
+    with open("C:/Users/leifa/Desktop/python shit/Selfbot/Final/codes.json", "r") as p:
+        codeList = json.load(p)
+    for i in codeList:
+        if int(codeList[i]) == int(price):
+            count+=1
+    if count > 0:
+        return count
+    else:
+        return -1
+        print("Invalid price. No codes found")
+
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=INSERT GUILD ID HERE))
+    await tree.sync(guild=discord.Object(id=YOUR GUILDID HERE))
     print(f'Bot User: {client.user}')
 
-@tree.command(name = "setcookie", description = "sets the cookie of the account with robux", guild=discord.Object(id=806917670473170984))
-@app_commands.checks.has_role(INSERT ROLE ID HERE)
+@tree.command(name = "setcookie", description = "sets the cookie of the account with robux", guild=discord.Object(id=YOUR GUILDID HERE))
+@app_commands.checks.has_role(YOUR ADMIN ROLE HERE)
 async def setcookie(interaction, cookie: str):
     LocalUser.SetCookie(cookie)
     global requestCookie
@@ -40,18 +53,24 @@ async def setcookie(interaction, cookie: str):
     await interaction.response.send_message("Cookie set.", ephemeral = True)
     await interaction.followup.send(Utils.CheckCookie(LocalUser.RawCookie), ephemeral = True)
 
-@tree.command(name = "redeemrbx", description = "Redeems your robux", guild=discord.Object(id=INSERT GUILD ID HERE))
+@tree.command(name = "countcodes", description = "counts all available codes with a certain value", guild=discord.Object(id=YOUR GUILDID HERE))
+@app_commands.checks.has_role(YOUR ADMIN ROLE HERE)
+async def countcodes(interaction, value: str):
+    if getCodes(value) > 0:
+        await interaction.response.send_message("There are currently %s codes worth that amount" % getCodes(value))
+    else:
+        await interaction.response.send_message("Invalid price. There are no codes with that price")
+
+
+@tree.command(name = "redeemrbx", description = "Redeems your robux", guild=discord.Object(id=YOUR GUILDID HERE))
 async def redeemrbx(interaction, code: str, passid: int):
     with open("C:/Users/leifa/Desktop/python shit/Selfbot/Final/codes.json", "r") as p:
         codeList = json.load(p)
     if codeList[code] == getPassPrice(passid):
-        for i, obj in enumerate(codeList):
-            if obj == code:
-                index = i
         codeList.pop(code)
         with open("C:/Users/leifa/Desktop/python shit/Selfbot/Final/codes.json", "w") as p:
             p.write(json.dumps(codeList, indent=2)) 
-        await interaction.response.send_message("valid pass. Buying item", ephemeral=True)
+        await interaction.response.send_message("Valid pass. Buying item", ephemeral=True)
 
         session = requests_html.HTMLSession()
         price_in_robux = getPassPrice(passid)
@@ -73,20 +92,18 @@ async def redeemrbx(interaction, code: str, passid: int):
             'expectedSellerId': creator_id,
         }
         response = session.post(
-            f'https://economy.roblox.com/v1/purchases/products/{passid}',
+            f'https://`economy.roblox.com/v1/purchases/products`/{passid}',
             cookies=cookies,
             headers=headers,
             json=json_data,
         )
         if response.ok:
-            print("LETSGOOOOO")
+            print("Successful Purchase")
             print(response)
-            print(price_in_robux)
-            print(creator_id)
         else:
-            print("L")
+            print("Purchase Failed")
     else:
         await interaction.response.send_message("Invalid pass price", ephemeral=True)
 
 
-client.run('INSERT YOUR TOKEN HERE')
+client.run('YOUR TOKEN HERE')
